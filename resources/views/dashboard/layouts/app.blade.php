e0437efef04d773345aebcacefaddd0437b0a682
shirazi96 <bilal.hashone@gmail.com>
2021-07-27 19:22:37 +0500

    Updates and bugs fix

--- resources/views/dashboard/layouts/app.blade.php
+++ resources/views/dashboard/layouts/app.blade.php
@@ -19,8 +19,8 @@
 
     <!-- Preloader -->
     <div class="preloader flex-column justify-content-center align-items-center">
-      <img class="animation__wobble" src="{{ asset('admin_dashboard/assets/img/AdminLTELogo.png') }}" alt="AdminLTELogo"
-        height="60" width="60">
+      <img class="animation__wobble" src="{{ url('storage/settings/'.$appSettings->logo) }}" alt="CMS Logo" height="60"
+        width="60">
     </div>
 
     <!-- Navbar -->
